323fa2c83a4e21ac3f3a81d77c1becb1d4ddeaac
fabianalauro <fabilauro98@gmail.com>
2021-07-15 21:41:07 +0200

    esercizio 7 caricato

---
+++ E_07/prior.sh
@@ -0,0 +1,6 @@
+cp config.fcc config.0
+make
+./Monte_Carlo_NVT.exe 1 0
+
+# <./Monte_Carlo_NVT.exe> <instant> <ave>
+
---
+++ E_07/simulation.sh
@@ -0,0 +1,6 @@
+cp config.final config.0
+cp seed.out seed.in
+./Monte_Carlo_NVT.exe 0 1
+
+# <./Monte_Carlo_NVT.exe> <instant> <ave>
+
